b8564dcea153769e2e01a92cb47b5f421408e434
Timur <aisakhanovtimur84@gmail.com>
2026-08-25 22:55:08 +0500

    AETERNA: resolve remaining UI type contracts

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -26,7 +26,13 @@ const InputOTPSlot = React.forwardRef<
   React.ComponentPropsWithoutRef<"div"> & { index: number }
 >(({ index, className, ...props }, ref) => {
   const inputOTPContext = React.useContext(OTPInputContext);
-  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index];
+  const slot = inputOTPContext.slots[index];
+
+  if (!slot) {
+    return null;
+  }
+
+  const { char, hasFakeCaret, isActive } = slot;
 
   return (
     <div
